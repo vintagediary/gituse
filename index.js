872c866ec837444888cfd6e901083029e2d446bf
vintagediary <dsyadavnda@gmail.com>
2023-04-20 12:52:36 +0530

    js push

---
+++ index.js
@@ -0,0 +1,12 @@
+//variable declaration
+var a;
+//variable intialization 
+a=10;
+//to know variable type
+console.log(typeof a);
+var float =2.44;
+var t =true;
+var str='true';
+console.log(typeof float);
+console.log(t);
+console.log(typeof str);
